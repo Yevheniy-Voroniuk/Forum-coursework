5888c9732cacedcb3d868a7a2c93d999643b54cf
Yevheniy-Voroniuk <evhenii.voroniuk@gmail.com>
2023-05-24 13:49:38 +0300

    added a database and were written models

--- src/main/java/com/example/Forum/model/Post.java
+++ src/main/java/com/example/Forum/model/Post.java
@@ -1,11 +1,24 @@
 package com.example.Forum.model;
 
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Entity
+@Table(name = "posts")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "postId")
     private Long postId;
+    @Column(name="title", nullable = false, length = 50)
     private String title;
+    @Column(name="content", nullable = false, length = 500)
     private String content;
 }
+
--- src/main/java/com/example/Forum/model/Topic.java
+++ src/main/java/com/example/Forum/model/Topic.java
@@ -1,10 +1,22 @@
 package com.example.Forum.model;
 
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "topics")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "topicId")
     private Long topicId;
+    @Column(name = "title", nullable = false, length = 50)
     private String title;
-
 }
+
