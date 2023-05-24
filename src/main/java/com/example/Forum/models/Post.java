c9bffd20bf7c8966d82cb1cebe74a4bd546709db
Yevheniy-Voroniuk <evhenii.voroniuk@gmail.com>
2023-05-24 22:05:35 +0300

    javax has been replaced with jakarta. replaced mysql dependency. updated application.properties

--- src/main/java/com/example/Forum/models/Post.java
+++ src/main/java/com/example/Forum/models/Post.java
@@ -1,5 +1,6 @@
 package com.example.Forum.models;
 
+import com.example.Forum.models.Topic;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,10 +16,14 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "postId")
     private Long postId;
-    @Column(name="title", nullable = false, length = 50)
+
+    @Column(name = "title", nullable = false, length = 50)
     private String title;
-    @Column(name="content", nullable = false, length = 500)
+
+    @Column(name = "content", nullable = false, length = 500)
     private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "topicId")
+    private Topic topic;
 }
-
-
--- src/main/java/com/example/Forum/models/Topic.java
+++ src/main/java/com/example/Forum/models/Topic.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.*;
+
 @Entity
 @Table(name = "topics")
 @Data
@@ -15,7 +17,10 @@ public class Topic {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "topicId")
     private Long topicId;
+
     @Column(name = "title", nullable = false, length = 50)
     private String title;
-}
 
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL)
+    private List<Post> posts;
+}
